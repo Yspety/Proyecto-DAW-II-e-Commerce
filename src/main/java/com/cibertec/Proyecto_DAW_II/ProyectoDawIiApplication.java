f82899e84b56cac67192609fa57dee2403f1059c
Billy Cristopher Muñoz Segura <billy.m.s@hotmail.com>
2026-05-15 13:29:50 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/cibertec/Proyecto_DAW_II/ProyectoDawIiApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.Proyecto_DAW_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/Proyecto_DAW_II/ProyectoDawIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.Proyecto_DAW_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
